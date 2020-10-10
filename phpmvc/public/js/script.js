02960811022fcf994eed1350d6586ec778e3daa7
rifqifahrezi <55418191+rifqifahrezi@users.noreply.github.com>
2020-10-10 19:38:42 +0700

    Add files via upload

---
+++ phpmvc/public/js/script.js
@@ -0,0 +1,29 @@
+$(function() {
+
+
+	$('.tombolTambahData').on('click', function() {
+		$('#formModalLabel').html('Tambah Data Mahasiswa');
+		$('.modal-footer button[type=submit]').html('Tambah Data');
+	});	
+
+
+	$('.tampilModalUbah').on('click', function() {
+		$('#formModalLabel').html('Ubah Data Mahasiswa');
+		$('.modal-footer button[type=submit]').html('Ubah Data');
+
+		const id = $(this).data('id');
+
+		$.ajax({
+			url: 'https://localhost/phpmvc/public/mahasiswa/getubah',
+			data: {id : id},
+			method: 'post',
+			dataType: 'json',
+			success: function(data) {
+				console.log(data);
+			}
+		});
+
+	});
+
+
+});
